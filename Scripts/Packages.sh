6b07f8f82effabb6ee2e429b62d3262983477a98
shaoyf <shaoyifan@live.com>
2025-10-17 21:35:27 +0800

    ci

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -70,8 +70,7 @@ UPDATE_PACKAGE "partexp" "sirpdboy/luci-app-partexp" "main"
 UPDATE_PACKAGE "qbittorrent" "sbwml/luci-app-qbittorrent" "master" "" "qt6base qt6tools rblibtorrent"
 UPDATE_PACKAGE "qmodem" "FUjr/QModem" "main"
 UPDATE_PACKAGE "quickfile" "sbwml/luci-app-quickfile" "main"
-UPDATE_PACKAGE "viking" "VIKINGYFY/packages" "main" "" "luci-app-timewol luci-app-wolplus"
-UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
+UPDATE_PACKAGE "van" "shaoyifan/packages" "main" "" "luci-app-adguardhome adguardhome luci-app-athena-led"
 
 #更新软件包版本
 UPDATE_VERSION() {
